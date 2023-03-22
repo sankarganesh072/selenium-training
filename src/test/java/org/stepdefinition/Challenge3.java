c1c9a23f735632c1f094a9fdc54df649419ee8e8
sankarganesh <sankarganesh072@gmail.com>
2023-03-22 07:35:39 -0700

    challenge3 added

---
+++ src/test/java/org/stepdefinition/Challenge3.java
@@ -0,0 +1,26 @@
+package org.stepdefinition;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Challenge3 {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver","D:\\work\\Screenshot\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		TakesScreenshot tt = (TakesScreenshot)driver;
+		File img = tt.getScreenshotAs(OutputType.FILE);
+		File f = new File("D:\\work\\Screenshot\\Storage\\ama.png");
+		FileUtils.copyFile(img, f);
+
+	}
+
+}
